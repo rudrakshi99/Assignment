cef1a5964284838cbe4c9d844ebb68272b1cd1d5
rudrakshi99 <sonirudrakshi99@gmail.com>
2021-07-12 11:07:43 +0530

    added product app

---
+++ product/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
